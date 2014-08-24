e53fe7eb729af11672a3742ff14412738497d9dd
lyle <admin@acoolsip.com>
2014-08-24 09:09:05 -0500

    working on ventures.

--- index.php
+++ index.php
@@ -24,10 +24,8 @@
 						<h2 class="bl-icon bl-icon-about"><span>Curriculum Vitae</span></h2>
 					</div>
 					<div class="bl-content">
-						<h2>Resume of Lyle Mcclanahan</h2>
-						<p>This responsive layout is based on an initial grid of four boxes. Once a box is clicked, it gets resized to fullscreen and the other boxes scale down and fade out. In the work section we experiment with another transition which is to show a panel by making it appear from the bottom while scaling the current one down. To see it in action, open the work section and click on one of the portfolio items and navigate through them.</p>
-						<p>
-						</p>
+						<h2 class="panelheading">Resume of Lyle Mcclanahan</h2>
+						<p>This responsive layout is based on an initial grid of four boxes. Once a box is clicked, it gets resized to fullscreen and the other boxes scale down and fade out. In the ventures section we experiment with another transition which is to show a panel by making it appear from the bottom while scaling the current one down. To see it in action, open the ventures section and click on one of the portfolio items and navigate through them.</p>
 					</div>
 					<span class="bl-icon bl-icon-close"></span>
 				</section>
@@ -36,15 +34,36 @@
 						<h2 class="bl-icon bl-icon-works"><span>Ventures</span></h2>
 					</div>
 					<div class="bl-content">
-						<h2>Ventures&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-3 fa-desktop"></i></h2>
-						<p>Mung bean maize dandelion sea lettuce catsear bunya nuts ricebean tatsoi caulie horseradish pea.</p>
+						<h2 class="panelheading">Ventures&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-3 fa-desktop"></i></h2>
+						<p>Not so current ventures I've taken in the adventure to become a better web developer. More to come...</p>
 						<ul id="bl-work-items">
-							<li data-panel="panel-1"><a href="#"><img src="images/1.jpg" /></a></li>
-							<li data-panel="panel-2"><a href="#"><img src="images/2.jpg" /></a></li>
-							<li data-panel="panel-3"><a href="#"><img src="images/3.jpg" /></a></li>
-							<li data-panel="panel-4"><a href="#"><img src="images/4.jpg" /></a></li>
-						</ul>
-						<p>Illustrations by <a href="http://dribbble.com/isaac317/click">Isaac Montemayor</a></p>
+                                                    <?php
+							    //path to directory to scan
+							    $directory = getcwd() . "/images/thumbs/";
+							    $url_path = "images/large/";
+							    //get all image files with a .jpg extension.
+							    $images = glob($directory . "*.png");
+							    $i = 1;
+							    //print each file name
+							    foreach($images as $image){						    
+								$image = explode("/",$image);
+								$image = array_pop($image);
+								$img = $url_path . $image;
+								
+							      ?>
+							      
+								<li data-panel="panel-<?php echo $i; ?>">
+									<a href="#">
+										<img src="<?php echo $img; ?>" width="150" height="133" />
+                                                                                
+									</a>
+								</li>
+								
+							      <?							      
+								$i++;
+							    }
+                                                    ?>
+                                                </ul>						
 					</div>
 					<span class="bl-icon bl-icon-close"></span>
 				</section>
@@ -62,45 +81,32 @@
 						<h2 class="bl-icon bl-icon-contact"><span>Contact</span></h2>
 					</div>
 					<div class="bl-content">
-						<h2>Get in touch</h2>
+						<h2 class="panelheading">Get in touch</h2>
 						<p>Pinterest semiotics single-origin coffee craft beer thundercats irony, tumblr bushwick intelligentsia pickled. Narwhal mustache godard master cleanse street art, occupy ugh selfies put a bird on it cray salvia four loko gluten-free shoreditch. Occupy american apparel freegan cliche. Mustache trust fund 8-bit jean shorts mumblecore thundercats. Pour-over small batch forage cray, banjo post-ironic flannel keffiyeh cred ethnic semiotics next level tousled fashion axe. Sustainable cardigan keytar fap bushwick bespoke.</p>
 					</div>
 					<span class="bl-icon bl-icon-close"></span>
 				</section>
 				<!-- Panel items for the works -->
 				<div class="bl-panel-items" id="bl-panel-work-items">
-					<div data-panel="panel-1">
+                                    <?php
+                                    for($p=1; $p<$i; $p++){
+                                    ?>
+					<div data-panel="panel-<?php echo $p; ?>">
 						<div>
-							<img src="images/1.jpg" />
+                                                        <h2 class="panelheading">Panel Heading</h2>
+							<img src="<?php echo 'images/large/'.$p.'.png'; ?>" />
 							<h3>Fixie bespoke</h3>
 							<p>Iphone artisan direct trade ethical Austin. Fixie bespoke banh mi ugh, deep v vinyl hashtag. Tumblr gentrify keffiyeh pop-up iphone twee biodiesel. Occupy american apparel freegan cliche. Mustache trust fund 8-bit jean shorts mumblecore thundercats. Pour-over small batch forage cray, banjo post-ironic flannel keffiyeh cred ethnic semiotics next level tousled fashion axe. Sustainable cardigan keytar fap bushwick bespoke.</p>
 						</div>
-					</div>
-					<div data-panel="panel-2">
-						<div>
-							<img src="images/2.jpg" />
-							<h3>Chillwave mustache</h3>
-							<p>Squid vinyl scenester literally pug, hashtag tofu try-hard typewriter polaroid craft beer mlkshk cardigan photo booth PBR. Chillwave 90's gentrify american apparel carles disrupt. Pinterest semiotics single-origin coffee craft beer thundercats irony, tumblr bushwick intelligentsia pickled. Narwhal mustache godard master cleanse street art, occupy ugh selfies put a bird on it cray salvia four loko gluten-free shoreditch.</p>
-						</div>
-					</div>
-					<div data-panel="panel-3">
-						<div>
-							<img src="images/3.jpg" />
-							<h3>Austin hella</h3>
-							<p>Ethical cray wayfarers leggings vice, seitan banksy small batch ethnic master cleanse. Pug chillwave etsy, scenester meh occupy blue bottle tousled blog tonx pinterest selvage mixtape swag cosby sweater. Synth tousled direct trade, hella Austin craft beer ugh chambray.</p>
-						</div>
-					</div>
-					<div data-panel="panel-4">
-						<div>
-							<img src="images/4.jpg" />
-							<h3>Brooklyn dreamcatcher</h3>
-							<p>Fashion axe 90's pug fap. Blog wayfarers brooklyn dreamcatcher, bicycle rights retro YOLO. Wes anderson lomo 90's food truck 3 wolf moon, twee jean shorts. Iphone small batch twee wolf yr before they sold out. Brooklyn echo park cred, portland pug selvage flannel seitan tousled mcsweeney's.</p>
-						</div>
-					</div>
+					</div>                                    
+                                    <?php
+                                    }
+                                    ?>
 					<nav>
 						<span class="bl-next-work">&gt; Next Project</span>
 						<span class="bl-icon bl-icon-close"></span>
 					</nav>
+                                        
 				</div>
 			</div>
 			<div id="cleanpage">
