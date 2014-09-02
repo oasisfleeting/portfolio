cf6f1fff7d596cceebce511028a3a1a7625f72b3
lyle <admin@acoolsip.com>
2014-09-01 21:01:00 -0500

    adding contact form.

--- index.php
+++ index.php
@@ -17,11 +17,22 @@
                 <link rel="apple-touch-icon" sizes="120x120" href="icons/apple-touch-icon-120x120.png" />
                 <link rel="apple-touch-icon" sizes="144x144" href="icons/apple-touch-icon-144x144.png" />
                 <link rel="apple-touch-icon" sizes="152x152" href="icons/apple-touch-icon-152x152.png" />		
-		<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css" />
+		<!--<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css" />-->
+                <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+                
+                
+<link rel="stylesheet" type="text/css" href="css/form.css" />                 
+
+<!--                               
 		<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css"/>
+-->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/fonts.css"/>
+                
 		<link rel="stylesheet" type="text/css" href="css/blocks.css" />
 		
+                <script type="text/javascript" src="js/modernizr.custom.js"></script>
 	</head>
 	<body ng-app="lylesPortfolio">
 		<div class="container-fluid">
@@ -42,7 +53,7 @@
 					</div>
 					<div class="bl-content">
 						<h2 class="panelheading">Ventures&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-3 fa-desktop"></i></h2>
-						<p>Not so current ventures I've taken in the adventure to become a better web developer. More to come...</p>
+						<p>Not so current ventures I've taken in the adventure to become a better web developer. New stuff to come...</p>
 						<ul id="bl-work-items">
                                                     <?php
 							    //path to directory to scan
@@ -89,13 +100,124 @@
 					<div class="bl-box">
 						<h2 class="bl-icon bl-icon-contact"><span>Contact</span></h2>
 					</div>
-					<div class="bl-content">
-						<h2 class="panelheading">Get in touch</h2>
-						<p>Pinterest semiotics single-origin coffee craft beer thundercats irony, tumblr bushwick intelligentsia pickled. Narwhal mustache godard master cleanse street art, occupy ugh selfies put a bird on it cray salvia four loko gluten-free shoreditch. Occupy american apparel freegan cliche. Mustache trust fund 8-bit jean shorts mumblecore thundercats. Pour-over small batch forage cray, banjo post-ironic flannel keffiyeh cred ethnic semiotics next level tousled fashion axe. Sustainable cardigan keytar fap bushwick bespoke.</p>
+					<div class="bl-content nooverflow" id="cform">
+						<h2 class="panelheading">Get in touch&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-3 fa-envelope"></i></h2>
+                                                <p>
+                                                    Use your keyboard to navigate the form. Press enter when finished filling out each form field. At the end you will be able to review what your entries. 
+                                                </p>
+                                                
+                                                        
+                                                        <form id="myform" class="fs-form fs-form-full" autocomplete="off">
+                                                                <ol class="fs-fields">
+                                                                        <li>
+                                                                                <label class="fs-field-label fs-anim-upper" for="q1">What's your name?</label>
+                                                                                <i class="fs-anim-upper required fa fa-asterisk"></i>
+                                                                                <input class="fs-anim-lower" id="q1" name="q1" type="text" placeholder="Lyle Mcclanahan" required/>                                                                                
+                                                                        </li>
+                                                                        <li>
+                                                                                <label class="fs-field-label fs-anim-upper" for="q2" data-info="We won't send you spam, we promise...">What's your email address?</label>
+                                                                                <i class="fs-anim-upper required fa fa-asterisk"></i>
+                                                                                <input class="fs-anim-lower" id="q2" name="q2" type="email" placeholder="lyle@lylemcclanahan.com" required/>
+                                                                                
+                                                                        </li>
+                                                                        <li data-input-trigger>
+                                                                                <label class="fs-field-label fs-anim-upper" data-info="This will help us know what kind of service you need">How can we make your life more simple?</label>
+                                                                                <div class="fs-radio-group fs-radio-custom clearfix fs-anim-lower">
+                                                                                        <span>
+                                                                                            <input id="q3e" name="q3" type="radio" value="custom"/>
+                                                                                            <i class="fa fa-github-alt fa-4x"></i>
+                                                                                            <label for="q3f" class="radio-custom">Customized</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3g" name="q3" type="radio" value="hosting"/>
+                                                                                            <i class="fa fa-sitemap fa-4x"></i>
+                                                                                            <label for="q3g" class="radio-hosting">Web Hosting</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3h" name="q3" type="radio" value="maintenance"/>
+                                                                                            <i class="fa fa-wrench fa-4x"></i>
+                                                                                            <label for="q3f" class="radio-maintenance">Maintenance</label>
+                                                                                        </span>                                                                                                                                                                                                                                                                          
+                                                                                        <span>
+                                                                                            <input id="q3b" name="q3" type="radio" value="conversion"/>
+                                                                                            <i class="fa fa-shopping-cart fa-4x"></i>
+                                                                                            <label for="q3b" class="radio-conversion">E-Commerce</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3c" name="q3" type="radio" value="social"/>
+                                                                                            <i class="fa fa-facebook-square fa-4x"></i>
+                                                                                            <label for="q3c" class="radio-social">Social Media</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3f" name="q3" type="radio" value="owncloud"/>
+                                                                                            <i class="fa fa-soundcloud fa-4x"></i>
+                                                                                            <label for="q3f" class="radio-owncloud">Eneterprise File Sharing</label>
+                                                                                        </span>                                                                                        
+                                                                                        <span>
+                                                                                            <input id="q3a" name="q3" type="radio" value="mobile"/>
+                                                                                            <i class="fa fa-mobile fa-4x"></i>
+                                                                                            <label for="q3a" class="radio-mobile">Mobile Apps</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3d" name="q3" type="radio" value="social"/>
+                                                                                            <i class="fa fa-wordpress fa-4x"></i>
+                                                                                            <label for="q3d" class="radio-blog">Blogging</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3e" name="q3" type="radio" value="mobile"/>
+                                                                                            <i class="fa fa-joomla fa-4x"></i>
+                                                                                            <label for="q3e" class="radio-cms">Content Management</label>
+                                                                                        </span>
+                                                                                        <span>
+                                                                                            <input id="q3i" name="q3" type="radio" value="other"/>
+                                                                                            <i class="fa fa-empire fa-4x"></i>
+                                                                                            <label for="q3i" class="radio-other">Other</label>
+                                                                                        </span> 
+                                                                                </div>
+                                                                                
+                                                                        </li>
+                                                                        <li data-input-trigger>
+                                                                                <label class="fs-field-label fs-anim-upper" data-info="We'll make sure to use it all over">Choose a color for your website.</label>
+                                                                                <select class="cs-select cs-skin-boxes fs-anim-lower">
+                                                                                        <option value="" disabled selected>Pick a color</option>
+                                                                                        <option value="#588c75" data-class="color-588c75">#588c75</option>
+                                                                                        <option value="#b0c47f" data-class="color-b0c47f">#b0c47f</option>
+                                                                                        <option value="#f3e395" data-class="color-f3e395">#f3e395</option>
+                                                                                        <option value="#f3ae73" data-class="color-f3ae73">#f3ae73</option>
+                                                                                        <option value="#da645a" data-class="color-da645a">#da645a</option>
+                                                                                        <option value="#79a38f" data-class="color-79a38f">#79a38f</option>
+                                                                                        <option value="#c1d099" data-class="color-c1d099">#c1d099</option>
+                                                                                        <option value="#f5eaaa" data-class="color-f5eaaa">#f5eaaa</option>
+                                                                                        <option value="#f5be8f" data-class="color-f5be8f">#f5be8f</option>
+                                                                                        <option value="#e1837b" data-class="color-e1837b">#e1837b</option>
+                                                                                        <option value="#9bbaab" data-class="color-9bbaab">#9bbaab</option>
+                                                                                        <option value="#d1dcb2" data-class="color-d1dcb2">#d1dcb2</option>
+                                                                                        <option value="#f9eec0" data-class="color-f9eec0">#f9eec0</option>
+                                                                                        <option value="#f7cda9" data-class="color-f7cda9">#f7cda9</option>
+                                                                                        <option value="#e8a19b" data-class="color-e8a19b">#e8a19b</option>
+                                                                                        <option value="#bdd1c8" data-class="color-bdd1c8">#bdd1c8</option>
+                                                                                        <option value="#e1e7cd" data-class="color-e1e7cd">#e1e7cd</option>
+                                                                                        <option value="#faf4d4" data-class="color-faf4d4">#faf4d4</option>
+                                                                                        <option value="#fbdfc9" data-class="color-fbdfc9">#fbdfc9</option>
+                                                                                        <option value="#f1c1bd" data-class="color-f1c1bd">#f1c1bd</option>
+                                                                                </select>                                                                                
+                                                                        </li>
+                                                                        <li>
+                                                                                <label class="fs-field-label fs-anim-upper" for="q4">Describe how you imagine your new website</label>
+                                                                                <textarea class="fs-anim-lower" id="q4" name="q4" placeholder="Describe here"></textarea>
+                                                                        </li>
+                                                                        <li>
+                                                                                <label class="fs-field-label fs-anim-upper" for="q5">What's your budget?</label>                                                                                
+                                                                                <input class="fs-mark fs-anim-lower" id="q5" name="q5" type="text" placeholder="1000" step="100" min="100"/>
+                                                                                <i class="dollars fs-anim-lower fa fa-usd"></i>
+                                                                        </li>
+                                                                </ol>
+                                                                <button class="fs-submit" type="submit">Send answers</button>
+                                                        </form>                                               
 					</div>
 					<span class="bl-icon bl-icon-close"></span>
 				</section>
-				<!-- Panel items for the works -->
+				<!-- Panel items for ventures -->
 				<div class="bl-panel-items" id="bl-panel-work-items">
                                     <?php
                                     for($p=1; $p<$i; $p++){
@@ -123,11 +245,36 @@
 			</div>
 		</div><!-- /container -->
 		
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.18/angular.min.js"></script>		
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js"></script>
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/angular.min.js"></script>				
 		<script type="text/javascript" src="js/ngportfolio.js"></script>
 		<script type="text/javascript" src="js/blocks.js"></script>
+		
+                
+                <script type="text/javascript" src="js/classie.js"></script>
+		<script type="text/javascript" src="js/selectFx.js"></script>
+		<script type="text/javascript" src="js/fullscreenForm.js"></script>
+		<script>
+			(function() {
+				var formWrap = document.getElementById( 'fs-form-wrap' );
+
+				[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {	
+					new SelectFx( el, {
+						stickyPlaceholder: false,
+						onChange: function(val){
+							document.querySelector('span.cs-placeholder').style.backgroundColor = val;
+						}
+					});
+				} );
+
+				new FForm( formWrap, {
+					onReview : function() {
+						classie.add( document.body, 'overview' ); // for demo purposes only
+					}
+				} );
+			})();
+		</script>
+                
 	</body>
 </html>
